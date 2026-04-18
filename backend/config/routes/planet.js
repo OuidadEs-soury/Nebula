e0474e182d9b30ac43764a8ce3b08b69e14ed046
Ouidad7 <widadelsouri@gmail.com>
2026-04-18 18:49:45 +0100

    Evolution Loop

--- backend/config/routes/planet.js
+++ backend/config/routes/planet.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const Planet = require("../models/Planet");
-
-router.get("/", async (req, res) => {
-  const planets = await Planet.find();
-  res.json(planets);
-});
+const generateLore = require("../utils/generateLore");
 
 router.post("/", async (req, res) => {
-  const planet = new Planet(req.body);
+  const lore = generateLore(req.body.name);
+
+  const planet = new Planet({
+    ...req.body,
+    lore
+  });
+
   await planet.save();
+  res.json(planet);
+});
+
+router.post("/evolve/:id", async (req, res) => {
+  const planet = await Planet.findById(req.params.id);
+
+  planet.population += Math.floor(Math.random() * 500);
+  planet.resources.energy += 10;
+  planet.resources.minerals += 5;
+  planet.civilizationLevel += 1;
+
+  await planet.save();
+
   res.json(planet);
 });
 
